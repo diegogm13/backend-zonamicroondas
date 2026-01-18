5b80d61fe80c6f866f660a472efdde388d458d27
diegogm13 <dg660262@gmail.com>
2026-01-18 05:01:33 -0600

    cambios en el server.js

--- server.js
+++ server.js
@@ -61,17 +61,21 @@ const upload = multer({
   fileFilter
 });
 
-// Pool de conexiones a la BD (CON charset utf8mb4)
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'zonamicroondas',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: 3306,
   waitForConnections: true,
-  connectionLimit: 10,
+  connectionLimit: 5,
   queueLimit: 0,
-  charset: 'utf8mb4'
+  charset: 'utf8mb4',
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
+
 
 // ==================== NOTICIAS ====================
 
